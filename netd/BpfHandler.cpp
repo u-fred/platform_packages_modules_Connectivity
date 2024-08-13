08bff1955bca8bc233bcc0174e15df7e3a3fee9c
Fred Underwood <underwoodfred@proton.me>
2024-08-13 14:51:40 +1000

    drop multicast when lockdown VPN enabled

--- netd/BpfHandler.cpp
+++ netd/BpfHandler.cpp
@@ -128,7 +128,10 @@ static Status initPrograms(const char* cg2_path) {
         RETURN_IF_NOT_OK(attachProgramToCgroup(CGROUP_UDP6_SENDMSG_PROG_PATH,
                                     cg_fd, BPF_CGROUP_UDP6_SENDMSG));
 
-        if (bpf::isAtLeastKernelVersion(5, 4, 0)) {
+        // BPF_PROG_TYPE_CGROUP_SOCKOPT was introduced in 5.3, but
+        // https://github.com/torvalds/linux/commit/d8fe449a9c51a37d844ab607e14e2f5c657d3cf2 was
+        // added in 5.8, which is required.
+        if (bpf::isAtLeastKernelVersion(5, 8, 0)) {
             RETURN_IF_NOT_OK(attachProgramToCgroup(CGROUP_GETSOCKOPT_PROG_PATH,
                                         cg_fd, BPF_CGROUP_GETSOCKOPT));
             RETURN_IF_NOT_OK(attachProgramToCgroup(CGROUP_SETSOCKOPT_PROG_PATH,
@@ -139,6 +142,7 @@ static Status initPrograms(const char* cg2_path) {
             RETURN_IF_NOT_OK(attachProgramToCgroup(CGROUP_INET_RELEASE_PROG_PATH,
                                         cg_fd, BPF_CGROUP_INET_SOCK_RELEASE));
         }
+
     }
 
     if (bpf::isAtLeastKernelVersion(4, 19, 0)) {
@@ -166,7 +170,7 @@ static Status initPrograms(const char* cg2_path) {
         if (bpf::queryProgram(cg_fd, BPF_CGROUP_UDP4_SENDMSG) <= 0) abort();
         if (bpf::queryProgram(cg_fd, BPF_CGROUP_UDP6_SENDMSG) <= 0) abort();
 
-        if (bpf::isAtLeastKernelVersion(5, 4, 0)) {
+        if (bpf::isAtLeastKernelVersion(5, 8, 0)) {
             if (bpf::queryProgram(cg_fd, BPF_CGROUP_GETSOCKOPT) <= 0) abort();
             if (bpf::queryProgram(cg_fd, BPF_CGROUP_SETSOCKOPT) <= 0) abort();
         }
